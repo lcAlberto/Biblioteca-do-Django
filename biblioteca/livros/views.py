d74e43bb6aae8728dcaace6b9b73b8ef049d0d13
lcAlberto <lcalbertomatoso2012@gmail.com>
2025-03-30 22:32:46 -0300

    feature add user crud and authe api

--- biblioteca/livros/views.py
+++ biblioteca/livros/views.py
@@ -4,11 +4,14 @@
 from rest_framework import viewsets
 from .models import Livro, Emprestimo
 from .serializers import LivroSerializer, EmprestimoSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    permission_classes = [IsAuthenticated]
 
 class EmprestimoViewSet(viewsets.ModelViewSet):
     queryset = Emprestimo.objects.all()
     serializer_class = EmprestimoSerializer
+    permission_classes = [IsAuthenticated]
--- biblioteca/usuarios/views.py
+++ biblioteca/usuarios/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from .models import Usuario
+from .serializers import UsuarioSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
-# Create your views here.
+class UsersApi(APIView):
+
+    def get(self, request, pk=None):
+        if pk:
+            usuario = Usuario.objects.get(pk=pk)
+            serializer = UsuarioSerializer(usuario)
+            return Response(serializer.data)
+        else:
+            usuarios = Usuario.objects.all()
+            serializer = UsuarioSerializer(usuarios, many=True)
+            return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def put(self, request, pk):
+        usuario = Usuario.objects.get(pk=pk)
+        serializer = UsuarioSerializer(usuario, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        usuario = Usuario.objects.get(pk=pk)
+        usuario.delete()
+        return Response(status=204)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        refresh_token = request.data.get('refresh')
+        if refresh_token is None:
+            return Response({"detail": "Token de refresh não fornecido."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Logout realizado com sucesso."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response({"detail": "Token inválido."}, status=status.HTTP_400_BAD_REQUEST)
+
